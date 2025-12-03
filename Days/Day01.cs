dbfbe279ee38b3d1beb22e39784aab363a39b07b
Laurens de Swart <laurensdeswart@gmail.com>
2025-12-03 22:05:56 +0100

    Part 2

--- Days/Day01.cs
+++ Days/Day01.cs
@@ -32,7 +32,36 @@ public class Day01 : IDay
 
     public override string Part2(string input)
     {
-        // TODO: Implement Part 2
-        return "Not implemented";
+        var lines = GetInputFile();
+
+        var dial = 50;
+        var dialCount = 0;
+
+        foreach (var line in lines)
+        {
+            char direction = line[0];
+            int distance = int.Parse(line[1..]);
+
+            dialCount += distance / 100;
+            distance %= 100;
+
+            switch (direction)
+            {
+                case 'L':
+                    if (dial != 0 && dial - distance <= 0)
+                        dialCount++;
+                    dial -= distance;
+                    break;
+                case 'R':
+                    if (dial != 0 && dial + distance >= 100)
+                        dialCount++;
+                    dial += distance;
+                    break;
+            };
+
+            dial = (dial + 100) % 100;
+        }
+
+        return dialCount.ToString();
     }
 }
