ff61f874fb342338fa5b844b7965e975bcb26d53
Laurens de Swart <laurensdeswart@gmail.com>
2025-12-05 16:03:22 +0100

    Part 2

--- Days/Day02.cs
+++ Days/Day02.cs
@@ -21,7 +21,7 @@ public class Day02 : IDay
             int endLength = bounds[1].Length;
 
             var startHalf = startLength % 2 == 0 ? long.Parse(bounds[0][..(startLength / 2)]) : NthPowerOf10((startLength) / 2);
-            var endHalf = endLength % 2 == 0 ? long.Parse(bounds[1][..(endLength / 2)]) : NthPowerOf10((endLength - 1) / 2) - 1;
+            var endHalf = endLength % 2 == 0 ? long.Parse(bounds[1][..(endLength / 2)]) : NthPowerOf10(endLength / 2) - 1;
 
             if (endHalf < startHalf) continue;
 
@@ -40,8 +40,51 @@ public class Day02 : IDay
 
     public override string Part2(string input)
     {
-        // TODO: Implement Part 2
-        return "Not implemented";
+        var lines = GetInputFile();
+        var line = lines[0];
+        var ranges = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+        var divisionLookup = new List<List<int>>();
+
+        for (int i = 0; i < 20; i++)
+        {
+            divisionLookup.Add(GetDivisors(i));
+        }
+
+        long total = 0;
+
+        HashSet<long> seen = [];
+
+        foreach (var range in ranges)
+        {
+            var bounds = range.Split('-', StringSplitOptions.RemoveEmptyEntries);
+            long start = long.Parse(bounds[0]);
+            int startLength = bounds[0].Length;
+            long end = long.Parse(bounds[1]);
+            int endLength = bounds[1].Length;
+            
+            for (long i = start; i <= end; i++)
+            {
+                var str = i.ToString();
+                int length = str.Length;
+                var divisors = divisionLookup[length];
+                foreach (var divisor in divisors)
+                {
+                    var repeatCount = length / divisor;
+                    var repeatedSequence = string.Join("", Enumerable.Repeat(str[..divisor], repeatCount));
+                    var repeatedNumber = long.Parse(repeatedSequence);
+
+                    if (!seen.Contains(repeatedNumber) && repeatedNumber >= start && repeatedNumber <= end)
+                    {
+                        total += repeatedNumber;
+                        seen.Add(repeatedNumber);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return total.ToString();
     }
 
     private static long NthPowerOf10(long n)
@@ -52,5 +95,18 @@ public class Day02 : IDay
             result *= 10;
         }
         return result;
+    }
+
+    private static List<int> GetDivisors(int n)
+    {
+        var divisors = new List<int>();
+        for (int i = 1; i <= n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                divisors.Add(i);
+            }
+        }
+        return divisors;
     }
 }
